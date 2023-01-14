14a21f7cbd9bf22135d41d647fd8ddadedefa6bf
André Pedrosa <ped17006@byui.edu>
2023-01-13 23:31:16 -0300

    Creating new function that adds (not replace) data

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -13,6 +13,15 @@ export function getLocalStorage(key) {
 export function setLocalStorage(key, data) {
   localStorage.setItem(key, JSON.stringify(data));
 }
+// add data to local storage
+export function addToLocalStorage(key, data) {
+  // if data is empty (null) start an array, otherwise get storage data
+  let storageData = getLocalStorage(key) ?? [];
+  // add data to the end of storage data
+  storageData.push(data);
+  // save it
+  setLocalStorage(key, storageData);
+}
 // set a listener for both touchend and click
 export function setClick(selector, callback) {
   qs(selector).addEventListener("touchend", (event) => {
