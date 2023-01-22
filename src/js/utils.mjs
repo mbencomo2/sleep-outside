cf4e0e8e8a49ff17d38db883b7487bf84ba34297
André Pedrosa <ped17006@byui.edu>
2023-01-21 23:34:48 -0300

    Adding null coalescing operator for default value

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -7,7 +7,7 @@ export function qs(selector, parent = document) {
 
 // retrieve data from localstorage
 export function getLocalStorage(key) {
-  return JSON.parse(localStorage.getItem(key));
+  return JSON.parse(localStorage.getItem(key)) ?? [];
 }
 // save data to local storage
 export function setLocalStorage(key, data) {
