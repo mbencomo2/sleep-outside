4db2e9470b9b5262e62a1980c80c9518bf7f0536
ailenrmansilla <ailen.r.mansilla@gmail.com>
2023-01-25 22:06:34 -0500

    Calculated and displayed total in cart.

--- src/js/cart.js
+++ src/js/cart.js
@@ -4,6 +4,15 @@ function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  
+  // Calculate the total amount to pay for the current products in cart (if there are any)
+  let total = 0;
+  if (cartItems.length != 0) {
+    cartItems.forEach( item => total += item.FinalPrice);
+  }
+  // When calculated the final total, call the displayTotalCart() function
+  displayTotalCart(total);
+
 }
 
 function cartItemTemplate(item) {
@@ -23,6 +32,14 @@ function cartItemTemplate(item) {
 </li>`;
 
   return newItem;
+}
+
+function displayTotalCart(total){
+  // Display the HTML section "cart-footer" and show the total amount to pay for the items
+  document.querySelector(".cart-footer").style.display = "block";
+  let total_in_cart = document.getElementById("total-in-cart");
+  total_in_cart.innerHTML = "Total: $"+total;
+  console.log('In the function! $', total)
 }
 
 renderCartContents();
