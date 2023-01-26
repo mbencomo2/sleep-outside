1f77f6519526b418f0c9024e67916da9f7762201
mbencomo2 <matt.ibencomo@gmail.com>
2023-01-26 01:31:06 -0700

    use double quotes instead

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@ import ProductData from "./ProductData.mjs";
 import ProductListing from "./productList.mjs";
 
 const dataSource = new ProductData("tents");
-const listElement = document.querySelector('.product-list');
+const listElement = document.querySelector(".product-list");
 
 const prodList = new ProductListing("tents", dataSource, listElement);
 prodList.init();
