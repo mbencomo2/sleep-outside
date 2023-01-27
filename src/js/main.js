f8877439c7a2769062056439773da07b50248ab2
mbencomo2 <matt.ibencomo@gmail.com>
2023-01-27 14:41:29 -0700

    formatting

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,12 @@ import ProductListing from "./productList.mjs";
 
 const dataSource = new ProductData("tents");
 const listElement = document.querySelector(".product-list");
-const topProducts = ["880RR", "985RF", "985PR", "344YJ"]
+const topProducts = ["880RR", "985RF", "985PR", "344YJ"];
 
-const prodList = new ProductListing("tents", dataSource, listElement, topProducts);
+const prodList = new ProductListing(
+  "tents",
+  dataSource,
+  listElement,
+  topProducts
+);
 prodList.init();
