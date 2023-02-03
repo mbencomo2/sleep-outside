f1bb0ce6df97b7fc2c159d6537c278239172b163
ailenrmansilla <ailen.r.mansilla@gmail.com>
2023-02-03 00:27:53 -0500

    shows the total with two decimal places

--- src/js/cart.js
+++ src/js/cart.js
@@ -4,14 +4,14 @@ function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
-  
+
   // Calculate the total amount to pay for the current products in cart (if there are any)
   let total = 0;
   if (cartItems.length != 0) {
     cartItems.forEach( item => total += item.FinalPrice);
   }
   // When calculated the final total, call the displayTotalCart() function
-  displayTotalCart(total);
+  displayTotalCart(total.toFixed(2));
 }
 
 function removeFromCart(parent) {
@@ -66,7 +66,6 @@ function displayTotalCart(total){
   document.querySelector(".cart-footer").style.display = "block";
   let total_in_cart = document.getElementById("total-in-cart");
   total_in_cart.innerHTML = "Total: $"+total;
-  console.log('In the function! $', total)
 }
 
 renderCartContents();
