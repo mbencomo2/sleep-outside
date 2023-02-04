f98f0303bf9e19e2c8c6aaa1f81c67e672a0264d
mbencomo2 <matt.ibencomo@gmail.com>
2023-02-03 17:52:45 -0700

    load header/footer dynamically

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,14 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
+import { loadHeaderFooter, qs } from "./utils.mjs";
 
 const topProducts = ["880RR", "985RF", "985PR", "344YJ"];
 const dataSource = new ProductData("tents");
 const productList = new ProductList(
   "tents",
   dataSource,
-  document.querySelector(".product-list"),
+  qs(".product-list"),
   topProducts
 );
+loadHeaderFooter();
 productList.init();
--- src/js/product.js
+++ src/js/product.js
@@ -1,9 +1,10 @@
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
-import { getParam } from "./utils.mjs";
+import { getParam, loadHeaderFooter } from "./utils.mjs";
 
 const productId = getParam("product");
 const dataSource = new ProductData("tents");
 
 const product = new ProductDetails(productId, dataSource);
+loadHeaderFooter();
 product.init();
