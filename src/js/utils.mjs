44e27ff2abed0f70605944f52c198a7bbc46b998
mbencomo2 <matt.ibencomo@gmail.com>
2023-02-06 18:49:42 -0700

    getParam needs to accept any url parameter

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -33,10 +33,10 @@ export function setClick(selector, callback) {
   qs(selector).addEventListener("click", callback);
 }
 
-export function getParam(productId) {
+export function getParam(param) {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
-  const product = urlParams.get("product");
+  const product = urlParams.get(param);
   return product;
 }
 
