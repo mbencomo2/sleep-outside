e88a84aa9f4a09bf79b796b2b8dd32cdc839551b
mbencomo2 <ben22022@byui.edu>
2023-02-14 21:21:34 -0700

    allow the user to change the quantity of an item

--- src/js/product.js
+++ src/js/product.js
@@ -7,7 +7,7 @@ initPage();
 async function initPage() {
   const productId = getParam("product");
   const dataSource = new ProductData("tents");
-  
+
   const product = new ProductDetails(productId, dataSource);
 
   await loadHeaderFooter();
