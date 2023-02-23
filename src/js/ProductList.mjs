87ec629b2adffe7e2bfc76265aece7778b1d73cd
mbencomo2 <ben22022@byui.edu>
2023-02-22 22:55:44 -0700

    fix merge from main

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,10 +1,6 @@
 import { renderListWithTemplate, currencyFormatter } from "./utils.mjs";
 
 function productCardTemplate(product) {
-  const formatter = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-  });
   return `<li class="product-card">
     <a href="/product_pages/index.html?product=${product.Id}">
       <img
@@ -13,9 +9,15 @@ function productCardTemplate(product) {
       />
       <h3 class="card__brand">${product.Brand.Name}</h3>
       <h2 class="card__name">${product.NameWithoutBrand}</h2>
-      <p class="product-card__price">List Price: ${currencyFormatter(product.ListPrice)}</p>
-      <p class="product-card__price">Final Price: ${currencyFormatter(product.FinalPrice)}</p>
-      <p class="product-card__price">Total Discount: ${currencyFormatter(product.ListPrice - product.FinalPrice)}</p>
+      <p class="product-card__price">List Price: ${currencyFormatter(
+        product.ListPrice
+      )}</p>
+      <p class="product-card__price">Final Price: ${currencyFormatter(
+        product.FinalPrice
+      )}</p>
+      <p class="product-card__price">Total Discount: ${currencyFormatter(
+        product.ListPrice - product.FinalPrice
+      )}</p>
     </a>
   </li>`;
 }
