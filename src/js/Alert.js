9047efdc50061d62c3f0bfd9f2ff8d44c6ce718a
angelaslinker <angela.r.slinker@gmail.com>
2023-02-24 17:57:16 -0700

    Fixed alert message

--- src/js/Alert.js
+++ src/js/Alert.js
@@ -12,20 +12,23 @@ class Alert {
         this.render();
       } catch (error){}
     }
-  
-    render() {
-      const alertList = document.createElement('section');
-      alertList.classList.add('alert-list');
-      this.alerts.forEach(alert => {
-        const { message, background, color } = alert;
-        const alertElement = document.createElement('p');
-        alertElement.textContent = message;
-        alertElement.style.backgroundColor = background;
-        alertElement.style.color = color;
-        alertList.appendChild(alertElement);
-      });
-      document.body.appendChild(alertList);
-    }
+
+  render() {
+    const alertList = document.createElement('section');
+    alertList.classList.add('alert-list');
+    this.alerts.forEach(alert => {
+      const { message, background, color } = alert;
+      const alertElement = document.createElement('p');
+      alertElement.textContent = message;
+      alertElement.style.backgroundColor = background;
+      alertElement.style.color = color;
+      alertList.appendChild(alertElement);
+    });
+    const main = document.querySelector('main');
+    main.insertBefore(alertList, main.firstChild);
+  }
+
+    
   }
   
   export default Alert;
--- src/js/main.js
+++ src/js/main.js
@@ -6,11 +6,6 @@ pageInit();
 async function pageInit() {
   await loadHeaderFooter();
   updateCartNumIcon();
-}
-
-// Create a new instance of the Alert class
-async function pageInit() {
-
   const alert = new Alert();
   const mainElement = document.querySelector('main');
   mainElement.insertAdjacentHTML('afterbegin', alert.render());
